b5cde239fccbf3f810a21a551fab2147477aecee
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-11-07 15:13:54 +0200

    toimii

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert,FlatList, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [recepies, setRecepies] = useState([]);
+  const getRecipes= () => { 
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
+    .then(response=>  response.json())
+    .then(responseJson=>{ 
+      setRecepies(responseJson.meals)
+      console.log(recepies);
+    })
+    .catch(error=> { Alert.alert('Error'); });
+    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1,justifyContent:'flex-end'}}>
+      <TextInput
+      placeholder='keyword'
+      onChangeText={ keyword=> setKeyword(keyword) } 
+      value={keyword}
+      />
+      <Button 
+      title='Find recipes'
+      onPress={getRecipes}/>
+      </View>
+          <View style={{flex: 3}}>
+      <FlatList
+            style={{marginLeft: "5%"}}
+            keyExtractor={(item,  index) => index.toString()}
+            renderItem={({item}) =>
+              <View>
+                <Text style={{fontSize: 18 , fontWeight: "bold"}}>{item.strMeal}</Text>
+                <Image 
+                style={{  width:100, height:100 }}
+                source={{  uri: `${item.strMealThumb}/preview`}}/>
+              </View>}
+              data={recepies}
+          />
+          </View>
+
       <StatusBar style="auto" />
     </View>
   );
